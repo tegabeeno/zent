51ac8e4da0f89e43a963f3e60bf3282947fbd826
vyaesop <100709552+vyaesop@users.noreply.github.com>
2024-03-25 19:40:09 +0300

    Update settings.py

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -1,5 +1,3 @@
-
-
 from pathlib import Path
 
 import os
@@ -16,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3%y3laftm62q0zaj+s7#p-xqq9(&#q+)s8)p-&#&bz*0$!xu$0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
